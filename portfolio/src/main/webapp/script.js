00873a87b7fd267229a654ee7fd3f31f4bfbb28e
Cedric <cedriclee94@gmail.com>
2020-06-06 15:42:24 +0800

    Add code to link visitor to Cedric's github and linkedin pages when the images are clicked. Also, remove random message generator. Adjust styling on style.css too.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,21 +13,6 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
-
-/**
  * Generates a URL for a random image in the images directory and adds an img
  * element with that URL to the page.
  */
